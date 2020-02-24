bbbd9c563137f1b9b35ff7eacdfd4f2d9f912398
Krishna <krishna@FULL395-PRT.local>
2020-02-25 00:49:03 +0530

    sw update

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,30 @@
-const staticCacheName = "Status";
-const dynamicCacheName = "Status";
-const assets = ["/", "/index.html", "/js/script.js", "/styles.css"];
+var cacheName = "status-pwa";
+var filesToCache = ["/index.html", "/style.css", "/js/script.js"];
 
-// install event
-self.addEventListener("install", evt => {
-    //console.log('service worker installed');
-    evt.waitUntil(
-        caches.open(staticCacheName).then(cache => {
-            console.log("caching shell assets");
-            cache.addAll(assets);
+// Install
+self.addEventListener("install", function(e) {
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+        .catch(err => {
+            console.log(err);
         })
     );
 });
 
-// activate event
-self.addEventListener("activate", evt => {
-    //console.log('service worker activated');
-    evt.waitUntil(
-        caches.keys().then(keys => {
-            //console.log(keys);
-            return Promise.all(
-                keys
-                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-                .map(key => caches.delete(key))
-            );
+// Fetch
+self.addEventListener("fetch", function(e) {
+    e.respondWith(
+        caches
+        .match(e.request)
+        .then(function(response) {
+            return response || fetch(e.request);
         })
-    );
-});
-
-// fetch event
-self.addEventListener("fetch", evt => {
-    //console.log('fetch event', evt);
-    evt.respondWith(
-        caches.match(evt.request).then(cacheRes => {
-            return (
-                cacheRes ||
-                fetch(evt.request).then(fetchRes => {
-                    return caches.open(dynamicCacheName).then(cache => {
-                        cache.put(evt.request.url, fetchRes.clone());
-                        return fetchRes;
-                    });
-                })
-            );
+        .catch(err => {
+            console.log(err);
         })
     );
 });
