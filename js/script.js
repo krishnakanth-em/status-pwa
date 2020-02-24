f4b8b84245a6e02a310f196e06ee2d6d90f7849c
Krishna <krishna@FULL395-PRT.local>
2020-02-25 00:52:50 +0530

    sw update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./service-worker.js");
-}
+// if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker.register("./service-worker.js");
+// }
+
+navigator.serviceWorker
+    .register("./service-worker.js", { scope: "/" })
+    .then(function(registration) {
+        console.log("Service worker registered successfully");
+    })
+    .catch(function(e) {
+        console.error("Error during service worker registration:", e);
+    });
