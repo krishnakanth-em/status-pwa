4476f121f4fb49bf4e618ecabc312211481e4954
Krishna <krishna@FULL395-PRT.local>
2020-02-25 00:26:35 +0530

    organised folder

--- docs/service-worker.js
+++
@@ -1,9 +0,0 @@
-// Install SW
-self.addEventListener("install", evt => {
-    console.log("Service worker has been installed");
-});
-
-// Activate SW
-self.addEventListener("activate", evt => {
-    console.log("Service worker has been Activated");
-});
---
+++ service-worker.js
@@ -0,0 +1,30 @@
+var cacheName = "Status";
+var filesToCache = ["/index.html", "/style.css", "/js/script.js"];
+
+// Install
+self.addEventListener("install", function(e) {
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    );
+});
+
+// Fetch
+self.addEventListener("fetch", function(e) {
+    e.respondWith(
+        caches
+        .match(e.request)
+        .then(function(response) {
+            return response || fetch(e.request);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    );
+});
