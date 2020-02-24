e71bb7ed3d4caad826cbdcfd8a188cce5a9837ed
Krishna <krishna@FULL395-PRT.local>
2020-02-25 00:36:51 +0530

    test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("../service-worker.js")
+        .register("/service-worker.js")
         .then(reg => console.log("Service Worker Registred", reg))
         .catch(err => console.log("Service Worker Not Registred", err));
 }
