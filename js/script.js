5fa67e2658d48b6cb5111e00cf07897c8486e3b1
Krishna <krishna@FULL395-PRT.local>
2020-02-25 00:43:59 +0530

    path fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("/service-worker.js")
-        .then(reg => console.log("Service Worker Registred", reg))
-        .catch(err => console.log("Service Worker Not Registred", err));
+    navigator.serviceWorker.register("./service-worker.js");
 }
