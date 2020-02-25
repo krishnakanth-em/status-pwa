89496dff46ec2166a642b819c1b8ca47f6ebbfeb
Krishna <krishna@FULL395-PRT.local>
2020-02-25 23:34:15 +0530

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,12 @@
 const staticCacheName = "site-static";
-const assets = ["/index.html", "/script.js", "/style.css"];
+const dynamicCacheName = "site-dynamic-v1";
+const assets = [
+    "/index.html",
+    "/script.js",
+    "/style.css",
+    "/images/sample.jpg",
+    "/videos/video.mp4"
+];
 
 // install event
 self.addEventListener("install", evt => {
@@ -15,9 +22,32 @@ self.addEventListener("install", evt => {
 // activate event
 self.addEventListener("activate", evt => {
     //console.log('service worker activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(
+                keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
 });
 
 // fetch event
 self.addEventListener("fetch", evt => {
     //console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return (
+                cacheRes ||
+                fetch(evt.request).then(fetchRes => {
+                    return caches.open(dynamicCacheName).then(cache => {
+                        cache.put(evt.request.url, fetchRes.clone());
+                        return fetchRes;
+                    });
+                })
+            );
+        })
+    );
 });
